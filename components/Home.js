c7971050fa7c47f479f15a5a45216edbb33a141a
Kamran Fekri <fekri.kamran@gmail.com>
2018-06-04 11:24:17 -0400

    added new directories and Home file

---
+++ components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{fontWeight: 'bold', fontSize: 30}}>RideProof</Text>
+        <Text style={{fontStyle: 'italic', fontSize: 20}}>Making your Ride Sharing Experience Safer</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
