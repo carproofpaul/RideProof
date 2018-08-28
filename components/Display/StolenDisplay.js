0c5bca66b0788337e8fab4fa9b4db58ee7595ecf
KamranFekri <fekri.kamran@gmail.com>
2018-08-28 14:00:23 -0400

    added comments to stolen display

--- components/Display/StolenDisplay.js
+++ components/Display/StolenDisplay.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+/**
+ * Displays if the vehicle is reported stolen.
+ * NOTE: we didn't have documentation on this section.
+ *  We assumpted if it's not null, then it's stolen. This needs to be tested...
+ */
+
 const StolenDisplay = ({stolenEvents}) => {
 
     iconColour = stolenEvents=== null ? 'green' : 'red'
